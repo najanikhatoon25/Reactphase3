c9f7ec379d4e5ba58eecda9aa0bdb5d235e96d3a
Najani khatoon <najanikhatoon25@navgurukul.org>
2026-09-07 10:53:58 +0530

    counter app is ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ function App(){
 
   const[count,setCount]=useState(0)
   return(
-    <div className="bg-white flex items-center justify-center w-40 h-35 border border">
+    <div className="bg-white flex  flex-col items-center justify-center  w-64 border border-gray-300 rounded-xl p-6 shadow-md mx-auto mt-10">
       <h1 >Counter App</h1>
       <h2>Count:{count}</h2>
-      <button onClick={()=> setCount+1}>Increase</button>
-      <button onClick={()=> setCount-1}>Decrease</button>
+      <button className="bg-green-500 p-2 rounded-lg text-white m-2"onClick={()=> setCount(count+1)}>Increase</button>
+      <button className="bg-red-500 p-2 rounded-lg text-white m-2"onClick={()=> {if(count>0) setCount(count-1)}}>Decrease</button>
 
     </div>
   )
