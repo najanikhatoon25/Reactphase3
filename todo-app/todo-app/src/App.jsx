b55a3a8d4834bbe39e8d113955af59687ebdcb08
najanikhatoon25 <najanikhatoon25@navgurukul.org>
2026-09-10 15:46:27 +0530

    first folderadded

---
+++ todo-app/todo-app/src/App.jsx
@@ -0,0 +1,12 @@
+import  {useState} from "react"
+function App(){
+  return(
+    <div>
+      <div>
+        <h2>My Todo App</h2>
+        <input type="text" placeholder="Enter your task..."/>
+        <button>Add</button>
+      </div>
+    </div>
+  )
+}
